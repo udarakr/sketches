8cf4c96e707d22f4165196b90a13e6df9aef0e41
Udara Rathnayake <udarakr@gmail.com>
2014-10-26 03:02:52 -0700

    basic toggle and slider added

---
+++ axis200-rc/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>AXIS200 Control Dash</title>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.4/jquery.mobile-1.4.4.min.css" />
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.4/jquery.mobile-1.4.4.min.js"></script>
+</head>
+<body>
+<select name="flip-1" id="flip-1" data-role="slider">
+	<option value="off">Off</option>
+	<option value="on">On</option>
+</select>
+<input type="range" name="slider-mini" id="slider-mini" value="0" min="0" max="100" data-highlight="true" data-mini="true" /> 
+</body>
+</html>
